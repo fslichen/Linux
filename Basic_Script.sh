5533f4513b34bbe945466465f58cb38d024207c9
fslichen <fslichen863@gmail.com>
2017-05-14 09:17:38 +0800

    Add files via upload
    commit

---
+++ Basic_Script.sh
@@ -0,0 +1,8 @@
+echo "Ready to change the permission level of the current script."
+chmod 777 Basic_Script.sh
+echo "Ready to list the files"
+ls
+echo "Ready to print the working directory."
+pwd
+
+
