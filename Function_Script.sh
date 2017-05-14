b97b1445587416b74643fa92b4283d8090e9e671
fslichen <fslichen863@gmail.com>
2017-05-14 09:58:06 +0800

    Add files via upload
    commit

---
+++ Function_Script.sh
@@ -0,0 +1,8 @@
+echo "Ready to create a function."
+function anyFunction {
+  echo "Hello World"
+}
+echo "Ready to call the function."
+anyFunction
+
+
---
+++ If_Script.sh
@@ -0,0 +1,30 @@
+# Do not miss space inside [].
+echo "Check if the file If_Script.sh exists."
+if [ -f If_Script.sh ]
+then
+  echo "The file If_Script.sh exists."
+else
+  echo "The file If_Script.sh does not exist."
+fi
+
+echo "Check if the directory Application exists"
+if [ -d Application ]
+then
+  echo "Directory Application exists."
+else
+  echo "Directory Application does not exist."
+fi
+
+echo "Example of if-elif-else Statement"
+if [ -w If_Script.sh ]
+then 
+  echo "If_Script.sh is writable."
+elif [ -r If_Script.sh ]
+then
+  echo "If_Script.sh is readable."
+elif [ -x If_Script.sh ]
+then
+  echo "If_Script.sh is executable."
+else
+  echo "You cannot do anything to If_Script.sh."
+fi
